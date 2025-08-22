8216755faf0277c6b7d7f5c320144e7ab7bf2ea0
ualperen41 <alperenb.200021@gmail.com>
2025-08-22 16:56:31 +0300

    project finished

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ menuBtn.addEventListener("click", () => {
      // nav kısmını aç-kapa yapmak için open-nav classını ekle-çıkar
      nav.classList.toggle("open-nav");
 });
+
+document.addEventListener("DOMContentLoaded",function () {
+     const videos = document.querySelectorAll("video");
+     const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+               if (entry.isIntersecting) {
+                    entry.target.play();
+                    entry.target.addEventListener("ended", () => {
+                         entry.target.currentTime = 0;
+                         entry.target.play();
+                    });
+               } else {
+                    entry.target.compareDocumentPosition();
+               }
+          });
+     });
+     videos.forEach((video) => {
+          observer.observe(video);
+     });
+});
